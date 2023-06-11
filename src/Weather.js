761b690be1379b30b526f68fb7910248aeea4d2f
Simiso Sesedza <ngwenyasimiso7@gmail.com>
2023-06-11 20:17:24 +0200

    Added link to Github

--- src/Weather.js
+++ src/Weather.js
@@ -1,12 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 import "./Weather.css";
 
 export default function Weather() {
+  const [city, setCity] = useState("");
+  const [weather, setWeather] = useState({});
+
+  function displayWeather(response) {
+    setWeather({
+      temperature: response.data.main.temp,
+      description: response.data.weather[0].description,
+      wind: response.data.wind.speed,
+      humidity: response.data.main.humidity,
+      icon: ` https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`,
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=ac209dae1f283fb332a5bb7f50b0f468&units=metric`;
+    axios.get(url).then(displayWeather);
+  }
+
+  function changeCity(event) {
+    setCity(event.target.value);
+  }
+
   return (
     <div className="weather-app-wrapper">
       <div className="weather-app">
-        <form className="search-form">
+        <form className="search-form" onSubmit={handleSubmit}>
           <div className="row">
             <div className="col-9">
               <input
@@ -14,6 +38,7 @@ export default function Weather() {
                 placeholder="Type a city..."
                 className="form-control"
                 autocomplete="off"
+                onChange={changeCity}
               />
             </div>
             <div className="col-3">
@@ -52,12 +77,22 @@ export default function Weather() {
           <ul>
             <li>Precipitation: 0%</li>
             <ul>
-              <li>Humidity: 80%</li>
-              <li>Wind: 5km/h</li>
+              <li>Humidity: {weather.humidity}80%</li>
+              <li>Wind: {weather.wind}5km/h</li>
             </ul>
           </ul>
         </div>
       </div>
+      <small>
+        <a
+          href="https://github.com/Smie84/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Open-source code
+        </a>
+        by Simiso Sesedza
+      </small>
     </div>
   );
 }
